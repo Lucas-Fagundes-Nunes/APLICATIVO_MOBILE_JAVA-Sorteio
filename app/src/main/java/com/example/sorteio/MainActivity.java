ef4e260d58a3b4ef8eb8f7ee1250fbe1928a12df
LucasNunes <lucasfnunes6@gmail.com>
2023-10-16 16:31:22 -0300

    Adicionado sorteio de 0 a 10

--- app/src/main/java/com/example/sorteio/MainActivity.java
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sortearNumero(View view){
+
+        TextView texto = findViewById(R.id.resultado);
+        int x = new Random().nextInt(11);
+        texto.setText("Número: "+ x);
+
+    }
+
 }
